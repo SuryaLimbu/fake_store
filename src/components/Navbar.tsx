981bb7227d6908c3a72ee07ddd34605a3f5ef3f5
surya_limbu <kedemkanxa@gmail.com>
2023-08-14 21:19:45 +0545

    add mobile nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import { FiSearch, FiShoppingCart } from 'react-icons/fi';
 import { Link } from "react-router-dom";
 
 // 
-import {useCart } from "react-use-cart";
+import { useCart } from "react-use-cart";
 const Navbar = () => {
     const [categories, setCategories] = useState([]);
 
@@ -41,15 +41,11 @@ const Navbar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Categories</a></li>
-                        <li>
-                            <a>Parent</a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        {categories.map((category) => (
+                            <li key={category}>
+                                <Link to={`/categoryfilter/${category}`}>{category}</Link>
+                            </li>
+                        ))}
                     </ul>
                 </div>
                 <div className="navbar-center hidden lg:flex">
